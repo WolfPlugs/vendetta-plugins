46a07499ebe8764a19521680c16718ce51036272
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-03-11 15:30:22 -0400

    Nothing

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,29 @@
 import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { findByDisplayName, findByProps, findByStoreName } from "@vendetta/metro";
+import { after, before } from "@vendetta/patcher";
+
+const userStore = findByStoreName("")
+
+
+let unpatch;
+
 
 export default {
     onLoad: () => {
         logger.log("Hello world!");
+
+        // unpatch = before("ProfileBadges", userStore, args => {
+        //     console.log(args[0])
+
+        // })
+        // unpatch = after("ProfileBadges", userStore, args => {
+        //     console.log(args[0])
+        // })
+
+        console.log(userStore)
     },
     onUnload: () => {
         logger.log("Goodbye, world.");
+        unpatch?.();
     },
-    settings: Settings,
 }
